13618beae0fb93fc876ba8286de39209b1b768d7
Caiozz07 <caiobandfranca@gmail.com>
2024-10-11 11:20:09 -0300

    Add files via upload

---
+++ notasponderadas_2gti/NotasPonderadas.java
@@ -0,0 +1,10 @@
+
+package notasponderadas_2gti;
+
+public class NotasPonderadas {
+    public static void main(String[] args) {
+        GUI_NotasPonderadas gui_NotasPonderadas = new GUI_NotasPonderadas();
+        gui_NotasPonderadas.setVisible(true);
+        
+    }
+}
